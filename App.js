4e4849bdf3c536f9dcae7680814a33ffd4b280f8
Akshaya C L <145364327+AkshayaChandran@users.noreply.github.com>
2024-10-01 08:50:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import HomeComponent from "./modules/home";
+
+const Container=styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px 0 10px;
+  font-family: Montserrat;
+`;
+
+const Header = styled.span`
+  color:black;
+  font-size: 25px;
+  font-weight: bold;
+`;
+function App() {
+  return (
+    <Container>
+      <Header>Expense Tracker</Header>
+      <HomeComponent/>
+    </Container>
+  );
+}
+
+export default App;
